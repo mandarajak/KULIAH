f4337567ba8e85f93169e34379e1d0bd92f2ee7f
Manda Rajak <mandarajak8@gmail.com>
2025-08-26 11:44:04 +0900

    Biodata

---
+++ DART/flutter_application_1/Project/latihan.dart
@@ -0,0 +1,10 @@
+void main(){
+  int number = 20;
+  double pecahan = 20;
+  num angka = 20.50;
+
+  print (number);
+  print (pecahan);
+  print (angka);
+
+}
---
+++ DART/flutter_application_1/lib/latihan1.dart
@@ -0,0 +1,16 @@
+void main(){
+  String name = "Manda Rajak";
+  int age = 19;
+  double berat = 45;
+  num tinggi = 149.5;
+  dynamic Prodi = "Teknik Informatika";
+  bool isActive = false;
+
+  print ("name : $name");
+  print ("age : $age");
+  print ("berat : $berat");
+  print ("tinggi : $tinggi");
+  print ("Prodi : $Prodi") ;
+  print ("Mahasiswa ini statusnya : $isActive");
+
+}
