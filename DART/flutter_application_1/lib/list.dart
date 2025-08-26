8b088d87479230b7712646294bc503f8e6fbed00
Manda Rajak <mandarajak8@gmail.com>
2025-08-26 13:03:09 +0900

    Operator Aritmatika

---
+++ DART/flutter_application_1/lib/list.dart
@@ -0,0 +1,12 @@
+void main(List<String> args){
+List <String> buah = ["mangga", "apel", "jeruk", "semangka"];
+buah.addAll(["durian", "nanas"]);
+
+print(buah);
+print(buah.first);
+print(buah.indexOf(buah[2]="kiwi"));
+buah.sort((a,b) {
+   return a.compareTo(b);
+});
+print(buah);
+}
---
+++ DART/flutter_application_1/lib/set.dart
@@ -0,0 +1,21 @@
+void main(List<String> args){
+  
+Set<dynamic> angka = { 1, 2, 3 , 4 ,5};
+
+print (angka. length);
+
+print (angka. add(6));
+
+print (angka. remove (1));
+
+print (angka. contains(2));
+
+angka. clear();
+
+print (angka);
+
+print (angka. runtimeType);
+
+print (angka. toList(). runtimeType);
+
+}
